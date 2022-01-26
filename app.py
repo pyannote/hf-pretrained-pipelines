4f5cac9e256e00ead5a0d851caf35b0c26dc6601
Hervé Bredin <bredin@limsi.fr>
2022-01-26 13:55:49 +0100

    chore: clean-up and license

--- app.py
+++ app.py
@@ -1,25 +1,43 @@
-from huggingface_hub import HfApi
-import matplotlib.pyplot as plt
-import streamlit as st
-from pyannote.audio import Pipeline
-from pyannote.audio import Audio
-from pyannote.core import notebook, Segment
+# MIT License
+#
+# Copyright (c) 2022- CNRS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 import io
-import base64
-
-import streamlit.components.v1 as components
-
-
-from matplotlib.backends.backend_agg import RendererAgg
-
-_lock = RendererAgg.lock
-
 import base64
 import numpy as np
 import scipy.io.wavfile
 from typing import Text
+from huggingface_hub import HfApi
+import streamlit as st
+from pyannote.audio import Pipeline
+from pyannote.audio import Audio
+from pyannote.core import Segment
+
+import streamlit.components.v1 as components
+
+
 def to_base64(waveform: np.ndarray, sample_rate: int = 16000) -> Text:
     """Convert waveform to base64 data"""
+    waveform /= np.max(np.abs(waveform)) + 1e-8
     with io.BytesIO() as content:
         scipy.io.wavfile.write(content, sample_rate, waveform)
         content.seek(0)
@@ -27,16 +45,13 @@ def to_base64(waveform: np.ndarray, sample_rate: int = 16000) -> Text:
         b64 = f"data:audio/x-wav;base64,{b64}"
     return b64
 
-def normalize(waveform: np.ndarray) -> np.ndarray:
-    """Normalize waveform for better display in Prodigy UI"""
-    return waveform / (np.max(np.abs(waveform)) + 1e-8)
 
 PYANNOTE_LOGO = "https://avatars.githubusercontent.com/u/7559051?s=400&v=4"
 EXCERPT = 30.0
 
 st.set_page_config(
-    page_title="pyannote.audio pretrained pipelines",
-    page_icon=PYANNOTE_LOGO)
+    page_title="pyannote.audio pretrained pipelines", page_icon=PYANNOTE_LOGO
+)
 
 
 st.sidebar.image(PYANNOTE_LOGO)
@@ -49,13 +64,17 @@ Upload an audio file and the first {EXCERPT:g} seconds will be processed automat
 """
 )
 
-PIPELINES = [p.modelId for p in HfApi().list_models(filter="pyannote-audio-pipeline") if p.modelId.startswith("pyannote/")]
+PIPELINES = [
+    p.modelId
+    for p in HfApi().list_models(filter="pyannote-audio-pipeline")
+    if p.modelId.startswith("pyannote/")
+]
 
 audio = Audio(sample_rate=16000, mono=True)
 
 selected_pipeline = st.selectbox("", PIPELINES, index=0)
 
-with st.spinner('Loading pipeline...'):
+with st.spinner("Loading pipeline..."):
     pipeline = Pipeline.from_pretrained(selected_pipeline)
 
 uploaded_file = st.file_uploader("")
@@ -66,13 +85,15 @@ if uploaded_file is not None:
     except RuntimeError as e:
         st.error(e)
         st.stop()
-    waveform, sample_rate = audio.crop(uploaded_file, Segment(0, min(duration, EXCERPT)))
+    waveform, sample_rate = audio.crop(
+        uploaded_file, Segment(0, min(duration, EXCERPT))
+    )
     file = {"waveform": waveform, "sample_rate": sample_rate, "uri": uploaded_file.name}
 
-    with st.spinner('Running pipeline...'):
+    with st.spinner("Running pipeline..."):
         output = pipeline(file)
 
-
+    # THIS HTML/JS TEMPLATE SHOULD BE LOADED FROM A FILE (e.g. /assets/template.html)
     html_template = """
     <script src="https://unpkg.com/wavesurfer.js"></script>
     <script src="https://unpkg.com/wavesurfer.js/dist/plugin/wavesurfer.regions.min.js"></script>
@@ -95,14 +116,26 @@ if uploaded_file is not None:
     </script>
     """
 
-    colors = ["#ffd700", "#00ffff", "#ff00ff", "#00ff00", "#9932cc", "#00bfff", "#ff7f50", "#66cdaa"]
+    colors = [
+        "#ffd700",
+        "#00ffff",
+        "#ff00ff",
+        "#00ff00",
+        "#9932cc",
+        "#00bfff",
+        "#ff7f50",
+        "#66cdaa",
+    ]
     label2color = {label: color for label, color in zip(output.labels(), colors)}
 
-    BASE64 = to_base64(normalize(waveform.numpy().T))
-    REGIONS = "".join([
-        f"wavesurfer.addRegion({{start: {segment.start:g}, end: {segment.end:g}, color: '{label2color[label]}'}});" 
-        for segment, track, label in output.itertracks(yield_label=True)])
-    html = html_template.replace('BASE64', BASE64).replace('REGIONS', REGIONS)
+    BASE64 = to_base64(waveform.numpy().T)
+    REGIONS = "".join(
+        [
+            f"wavesurfer.addRegion({{start: {segment.start:g}, end: {segment.end:g}, color: '{label2color[label]}'}});"
+            for segment, _, label in output.itertracks(yield_label=True)
+        ]
+    )
+    html = html_template.replace("BASE64", BASE64).replace("REGIONS", REGIONS)
     components.html(html, height=200)
 
     with io.StringIO() as fp:
